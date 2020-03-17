e8e20d0b198d5b086ee663abff39ea8c5ca101d2
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-17 12:05:31 +0900

    버튼을 자바에서 "회원가입"으로 문구 변경

--- app/src/main/java/kr/co/tjoeun/a20200317_01_login/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_01_login/MainActivity.java
@@ -3,12 +3,27 @@ package kr.co.tjoeun.a20200317_01_login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+//    멤버변수 생성해야함!!!
+//    참조형 변수 Button을 만들어서 null로 세팅;
+    Button loginBtn = null;
+    TextView findPwTxt = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+
+//                로그인 버튼의 글자를 회원가입으로 바꾸자
+        loginBtn.setText("회원가입");
+//        비밀번호 찾기 텍스트뷰를 => JAVA에서 "비번 찾기" 로 변경
+//        findPwTxt 로 id를 부여하자.
+        findPwTxt = findViewById(R.id.findPwTxt);
+        findPwTxt.setText("비번 찾기");
     }
 }
